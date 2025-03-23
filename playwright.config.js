4c339647e4906c52b0dac38313f07ed803acc9a0
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-03-23 20:12:31 +0100

    last fix

--- playwright.config.js
+++ playwright.config.js
@@ -24,15 +24,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
   /* Configure projects for major browsers */
   projects: [
     {
@@ -72,10 +63,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5174/tester-a11y/',
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:5174/tester-a11y/',
+  },
 });
-
--- tests/cas-pratique-2.spec.js
+++ tests/cas-pratique-2.spec.js
@@ -5,7 +5,7 @@ test.describe('cas-pratique-2', () => {
   test('Est-ce que ce site est vocalisable dans la bonne langue ?', async ({
     page,
   }) => {
-    await page.goto('https://emma11y.github.io/tester-a11y/cas-pratique-2');
+    await page.goto('./cas-pratique-2');
 
     const lang = await page.getAttribute('html', 'lang');
 
